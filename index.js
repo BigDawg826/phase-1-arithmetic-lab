089ac043dc7fd7b2aed7954d6e1db90b555c5ed9
Darian Fultz <dfultz826@yahoo.com>
2022-11-07 13:10:20 -0500

    Assignment completed.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 18;
+const num4 = 7;
+let mod = num3 % num4;
+
+const max = Math.max(20, 12, 7);
